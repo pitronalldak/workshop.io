784e5719d1aea066972f0e37e69b1e3d68089c46
pitronalldak <ryabinin.p.b@gmail.com>
2017-03-28 03:00:21 +0800

    unfinished scroll on projects

--- src/components/projects/ProjectsScrollListComponent.js
+++ src/components/projects/ProjectsScrollListComponent.js
@@ -1,47 +1,57 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import { bindActionCreators } from 'redux';
-import ReactDOM from 'react-dom';
+import ScrollAnim from 'rc-scroll-anim';
 
 import ProjectsActions from '../../actions/projects';
 import ProjectComponent from './ProjectComponent';
 
 const projects = (require('../../assets/json/projects.json'));
+const ScrollOverPack = ScrollAnim.OverPack;
+ScrollAnim.scrollScreen.init({ loop: true, scrollInterval: 500, duration: 200 });
 
 class ProjectsScrollListComponent extends Component {
 
-    state = {position: 0};
-
-    componentWillMount () {
-        window.addEventListener('scroll', this.trackScroll)
-    }
-
-    componentWillUnmount () {
-        window.removeEventListener('scroll', this.trackScroll)
-    }
-
-    trackScroll = () => {
-        if (window.scrollY > this.state.position) {
-            window.scrollTo(0, this.state.position + 500);
-            this.setState({position: 500})
-        }
-        console.log(window.scrollY)
-    };
+    // renderScrollOverPackReplayOption = (id) => {
+    //     let ScrollOverPackOption;
+    //     if (id==0) {
+    //         return 'replay'
+    //     }
+    // }
+    //
+    // renderScrollOverPackAlwaysOption = (id, length) => {
+    //     let ScrollOverPackOption;
+    //     if (id==(length-1)) {
+    //         debugger
+    //         return '{false}'
+    //     }
+    // }
 
     render () {
 
         return (
             <div >
                 {projects.map(project =>
-                    <ProjectComponent
+                    <div
+                        id={'page'+project.id}
                         key={project.id}
-                        title={project.title}
-                        date={project.date}
-                        description={project.description}
-                        development={project.development}
-                        technologies={project.technologies}
-                        link={project.link}
-                    />
+                    >
+                        <ScrollOverPack
+                            key={project.id}
+                            playScale={1}
+                            location={'page'+project.id}
+                        >
+                            <ProjectComponent
+                                key={project.id}
+                                title={project.title}
+                                date={project.date}
+                                description={project.description}
+                                development={project.development}
+                                technologies={project.technologies}
+                                link={project.link}
+                            />
+                        </ScrollOverPack>
+                    </div>
                 )}
             </div>
         );
